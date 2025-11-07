9fb1cc1d047e662cd79185d3c76b157e2290fb5f
adem-dr <136200266+adem-dr@users.noreply.github.com>
2025-11-07 15:45:52 +0100

    TP4 JavaScript terminé

--- exo3/script.js
+++ exo3/script.js
@@ -1,7 +1,7 @@
 "use strict";
 var students = [
-  { id: 1000, nom: "jhon", prnom: "Doe", note: [14, 5] },
-  { id: 2000, nom: "Bob", prnom: "Carlton", note: [7, 1] },
+  { id: 1000, nom: "jhon", prenom: "Doe", note: [14, 5] },
+  { id: 2000, nom: "Bob", prenom: "Carlton", note: [7, 1] },
   { id: 3000, name: "Rayane", prenom: "Smith", note: [13, 3] },
 ]; //tableau d'objets vu dans le slide 40
 
